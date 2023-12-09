10b28c0198159220c0d001d2d65637d862299848
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2023-12-09 13:42:07 +0100

    Creating an interface to add a new task and automatically display existing ones

---
+++ public/scripts.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const taskForm = document.getElementById('taskForm');
+  const taskList = document.getElementById('taskList');
+
+  taskForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const taskName = document.getElementById('taskName').value;
+    const taskDescription = document.getElementById('taskDescription').value;
+
+    fetch('/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name: taskName, description: taskDescription })
+    })
+    .then(response => response.json())
+    .then(data => {
+      addTaskToList(data);
+      taskForm.reset();
+    })
+    .catch(error => console.error('Error:', error));
+  });
+
+  function addTaskToList(task) {
+    const li = document.createElement('li');
+    li.textContent = `${task.name}: ${task.description}`;
+    taskList.appendChild(li);
+  }
+
+  function loadTasks() {
+    fetch('/tasks')
+    .then(response => response.json())
+    .then(tasks => {
+      tasks.forEach(addTaskToList);
+    })
+    .catch(error => console.error('Error:', error));
+  }
+
+  loadTasks();
+});
--- routes/tasks.js
+++ routes/tasks.js
@@ -22,31 +22,29 @@ router.post('/tasks', async (req, res) => {
 });
 
 router.put('/tasks/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-      const task = await Task.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
-      if (!task) {
-        return res.status(404).json({ message: 'Task not found' });
-      }
-      res.status(200).json(task);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
+  try {
+    const { id } = req.params;
+    const task = await Task.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
     }
-  });
-  
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
 router.delete('/tasks/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-      const task = await Task.findByIdAndDelete(id);
-      if (!task) {
-        return res.status(404).json({ message: 'Task not found' });
-      }
-      res.status(200).json({ message: 'Task deleted successfully' });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+  try {
+    const { id } = req.params;
+    const task = await Task.findByIdAndDelete(id);
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
     }
-  });
-  
-  
+    res.status(200).json({ message: 'Task deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
