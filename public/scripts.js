ec2c7afc8e3417db5e6368a605ff5b3c67e69ebd
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2023-12-09 14:19:57 +0100

    support for changing status of task

--- public/scripts.js
+++ public/scripts.js
@@ -24,8 +24,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function addTaskToList(task) {
     const li = document.createElement('li');
-    li.textContent = `${task.name}: ${task.description} (${task.status})`;
+    li.textContent = `${task.name}: ${task.description} (${task.status}) `;
+    const statusButton = document.createElement('button');
+    statusButton.textContent = 'Change Status';
+    statusButton.onclick = function() { changeTaskStatus(task); };
+    
+    li.appendChild(statusButton);
     taskList.appendChild(li);
+  }
+
+  function changeTaskStatus(task) {
+    taskId = task._id
+    const currentStatus = task.status;
+    const nextStatus = currentStatus === 'open' ? 'in progress' : currentStatus === 'in progress' ? 'done' : 'open';
+    
+    fetch(`/tasks/${taskId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ status: nextStatus })
+    })
+    .then(response => response.json())
+    .then(updatedTask => {
+        clearTaskList()
+        loadTasks()
+    })
+    .catch(error => console.error('Error:', error));
+  }
+
+  function clearTaskList() {
+    const taskList = document.getElementById('taskList');
+    taskList.innerHTML = ''; // Wyczyść zawartość listy
   }
 
   function loadTasks() {
